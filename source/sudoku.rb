125a3787dc2984319198b05fc9bfc91a94324ac8
Lukas Jones <joneslukas21@gmail.com>
2017-05-17 17:44:46 -0700

    Made code slightly less ugly
    * Remove unneccessary comments
    * Remove extra lines
    * Adhere to better code format
    * Use proc instead of repetitive block passing
    * Clean up conditional logic
    * Remove unused code

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,63 +1,63 @@
 class Sudoku
   def initialize(board_string)
     @board = board_string.split("").each_slice(9).to_a
-
   end
 
-  def find_single_row(row_index, board_to_check_in = @board)
-    board_to_check_in[row_index]
+  def find_single_row(row_index)
+    @board[row_index]
   end
 
   def find_single_column(col_index, board_to_check_in = @board)
-    return_column = []
-    board_to_check_in.each {|row| return_column << row[col_index]}
-    return_column
+    board_to_check_in.map {|row| row[col_index]}
   end
 
   def find_box(row_index, col_index)
     row_array = []
-     case row_index
-     when (0..2)
-      (0..2).each {|index_number| row_array << find_single_row(index_number)}
-     when (3..5)
-      (3..5).each {|index_number| row_array << find_single_row(index_number)}
-     when (6..8)
-      (6..8).each {|index_number| row_array << find_single_row(index_number)}
-     end
+
+    find_row = Proc.new {|index_number| row_array << find_single_row(index_number)}
+
+    case row_index
+    when (0..2)
+      (0..2).each(&find_row)
+    when (3..5)
+      (3..5).each(&find_row)
+    when (6..8)
+      (6..8).each(&find_row)
+    end
+
     find_box_columns(col_index, row_array)
   end
 
   def find_box_columns(col_index, row_array)
     box_array = []
+
+    find_col = Proc.new {|index_number| box_array << find_single_column(index_number, row_array)}
+
     case col_index
      when (0..2)
-      (0..2).each {|index_number| box_array << find_single_column(index_number, row_array)}
+      (0..2).each(&find_col)
      when (3..5)
-      (3..5).each {|index_number| box_array << find_single_column(index_number, row_array)}
+      (3..5).each(&find_col)
      when (6..8)
-      (6..8).each {|index_number| box_array << find_single_column(index_number, row_array)}
+      (6..8).each(&find_col)
     end
+
     box_array.flatten
   end
 
   def check_cell(row_index, column_index)
+    cell = @board[row_index][column_index]
 
-      if @board[row_index][column_index] == "-"
-        check_unique = ["1","2","3","4","5","6","7","8","9"]
-        check_unique -= find_single_row(row_index)
-        check_unique -= find_single_column(column_index)
-        check_unique -= find_box(row_index, column_index)
-        if check_unique.length == 1
-          # p check_unique
-          @board[row_index][column_index] = check_unique.join("")
-        else
-          @board[row_index][column_index] = "-"
-        end
-      else
-        @board[row_index][column_index] = @board[row_index][column_index]
-      end
+    if cell == "-"
+      check_unique = ["1","2","3","4","5","6","7","8","9"]
+      check_unique -= find_single_row(row_index)
+      check_unique -= find_single_column(column_index)
+      check_unique -= find_box(row_index, column_index)
 
+      cell = check_unique[0] if check_unique.length == 1
+    end
 
+    cell
   end
 
   def solved?
@@ -75,24 +75,7 @@ class Sudoku
     to_s
   end
 
-
-  def board
-  end
-
-  # Returns a nicely formatted string representing the current state of the board
   def to_s
     @board.each {|row| puts row.join("  ")}
   end
 end
-
-
-board_string = File.readlines('sudoku_puzzles.txt').first.chomp
-
-# game = Sudoku.new(board_string)
-# game.solve
-# puts game
-
-new_game = Sudoku.new(board_string)
-# p new_game.check_cell(0,0)
-new_game.solve
-# p new_game.solved?
